8e6f80335c5ef11845a0e1a9d40bad6c52345f4b
dragon <dragonskynaut@gmail.com>
2019-10-15 17:11:43 +0800

    security1

---
+++ src/main/java/com/example/demo11/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.demo11;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String sayHello(){
+        return "Hello world";
+    }
+}
